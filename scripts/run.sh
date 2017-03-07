82c2f5befec568808ca0da0eee36981c635acc67
Ubuntu <ubuntu@ip-172-31-9-28.us-west-1.compute.internal>
2017-03-07 21:14:02 +0000

    Modify Script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-java -cp target/classes:target/dependency:target/dependency/*:config/ brainllc.penghaoruo.isp.MainClass
+java -Xmx16g -cp target/classes:target/dependency:target/dependency/*:config/ brainllc.penghaoruo.isp.MainClass
