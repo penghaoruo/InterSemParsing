901de5c95ea927738fe23ec4778658ddea79ae1f
hpeng7 <hpeng7@illinois.edu>
2017-06-14 15:37:06 -0700

    Add code for frames

---
+++ src/main/java/brainllc/penghaoruo/isp/Parameters.java
@@ -0,0 +1,7 @@
+package brainllc.penghaoruo.isp;
+
+public class Parameters {
+	public static String frame_map_file = "frame_data/vn-fn.xml";
+	public static String verb_frame_dir = "frame_data/propbank/data";
+	public static String nom_frame_dir = "frame_data/nombank";
+}
